3af3c0421396ad3d4c2df4525b934690dc0a6d2b
samin_yeaser <samin.yeaser@tigerit.com>
2025-02-10 01:55:03 -0500

    updated codebase

--- neuroLSH.py
+++ neuroLSH.py
@@ -45,10 +45,3 @@ def run(params):
             run_kmkahip(height, opt, dataset, queryset, neighbors)
 
 
-info={
-    'data_loader': data_extractor.get_data_sift,
-    'graph_file': '-0-knn.graph',
-    'dataset': 'sift-small',
-    'k_graph': 100
-}
-run(info)
---
+++ scripts/test-1.py
@@ -0,0 +1,11 @@
+from .. import neuroLSH, data_extractor
+infos=[
+{
+    'data_loader': data_extractor.get_data_sift,
+    'graph_file': '-0-knn.graph',
+    'dataset': 'sift-small',
+    'k_graph': 100
+}
+]
+for info in infos:
+    neuroLSH.run(info)
